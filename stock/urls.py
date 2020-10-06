b9f63cb3e7c5b66c3fd23404d033e1c77a00a78a
Teep <posathip_ong@hotmail.com>
2020-10-06 17:51:20 +0700

    first commit

--- stock/urls.py
+++ stock/urls.py
@@ -5,4 +5,6 @@ urlpatterns=[
     path('',views.index, name='index'),
     path('about',views.about,name='about'),
     path('contract',views.contract,name='contract'),
+    path('random',views.random,name='random'),
+    path('signup',views.signup,name='signup'),
 ]
--- stock/views.py
+++ stock/views.py
@@ -4,12 +4,18 @@ from .models import Product
 
 # Create your views here.
 def index(request):
-    products = Product.objects.all()
-    return render(request,'frontend/index.html',{'products':products})
+    
+    return render(request,'frontend/index.html')
 
 def about(request):
     return render(request,'frontend/about.html')
 
 def contract(request):
     return render(request,'frontend/contract.html')    
+
+def signup(request):
+    return render(request,'frontend/signup.html')    
+ 
+def random(request):
+    return render(request,'frontend/random.html')    
 
